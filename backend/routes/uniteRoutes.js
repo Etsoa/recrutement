cbfdf5cd9e348dc7d8b38c1376ce76229273a97c
Tsiky-Rakotonirina <tsikyrakotonirina20@gmail.com>
2025-09-18 21:26:21 +0300

    uniteRoutes

---
+++ backend/routes/uniteRoutes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const uniteController = require('../controllers/uniteController');
+
+// liste annonce(components)
+router.get('/annonces', uniteController.getAllAnnonces);
+// fiche annonce
+router.get('/annonce', uniteController.getAnnonceById);
+// table envoi qcm candidat(email)
+router.post('/create/qcm_candidat', uniteController.createQcmCandidat);
+// table unite_entretien, status_unite_entretien(a venir id=1), envoi email 
+router.post('create/unite_entretien', uniteController.createUniteEntretien);
+
+// liste d entretiens par jour
+router.get('unite_entretiens_jour', uniteController.getAllUniteEntretiensParJour);
+// modifier entretien unite a la date
+router.post('update/date/unite_entretien', uniteController.updateDateUniteEntretien);
+// ajouter dans status_unite_entretien (termine id=2/en attente de validation id=3)
+router.post('update/status/unite_entretien', uniteController.updateStatusUniteEntretien);
+// ajouter score dans score_unite_entretien
+router.post('create/score/unite_entretien', uniteController.createScoreUniteEntretien);
+
+// suggestion a la RH
+router.post('/suggest/rh', uniteController.suggestToRh);
+// liste suggestions
+router.get('/suggest/', uniteController.getAllRhSuggestions);
+
+module.exports = router;  
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const publicRoutes = require('./routes/publicRoutes');
+const uniteRoutes = require('./routes/uniteRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/public', publicRoutes);
+app.use('/api/unite', uniteRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
